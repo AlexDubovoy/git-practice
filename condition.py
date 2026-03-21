add859f8d7732ce0763ff631d717a466c7c6a1d3
AlexDubovoy <dubovoy.alex91@gmail.com>
2026-03-21 19:47:15 +0300

    add condition.py

--- condition.py
+++ condition.py
@@ -1,22 +1,15 @@
-# 1. Сумма чётных от 1 до 100
-print("Сумма чётных от 1 до 100:", sum(range(2, 101, 2)))
+numbers = []
+current = 0
 
-# 2. Квадраты нечётных от 1 до 10
-print("Квадраты нечётных 1 до 10:", [x**2 for x in range(1, 11, 2)])
-
-# 3. Счётчик чисел до отрицательного
-count = 0
 print("Вводите числа (отрицательное — стоп):")
 
-number = 0
-
-while number >= 0:
+while current >= 0:
     try:
-        enter = input("→ ").strip()
-        number = int(enter)
-        if number >= 0:
-            count += 1
+        user_input = input("→ ").strip()
+        current = int(user_input)
+        numbers.append(current)
     except ValueError:
         print("Нужно ввести целое число")
 
-print(f"Введено неотрицательных чисел: {count}")
+print(f"Всего введено чисел: {len(numbers)}")
+print("Все числа:", numbers)
