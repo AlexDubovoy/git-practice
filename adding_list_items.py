479e0b3ac4612136b188d6e7d85411f78f6f041f
AlexDubovoy <dubovoy.alex91@gmail.com>
2026-03-25 17:22:31 +0300

    add adding_list_items.py

--- adding_list_items.py
+++ adding_list_items.py
@@ -1,11 +1,20 @@
-list_num1 = [122, 227, 339, 722, 114]
-list_num2 = [121, 212, 483, 777]
+def sum_lists(list1, list2):
+    """
+    Складывает соответствующие элементы 2х списков.
+    Если списки разной длины, недостающие элементы считаются как 0.
+    """
+    max_len = max(len(list1), len(list2))
 
-max_len = max(len(list_num1), len(list_num2))
+    extended_list1 = list1 + [0] * (max_len - len(list1))
+    extended_list2 = list2 + [0] * (max_len - len(list2))
 
-list1 = list_num1 + [0] * (max_len - len(list_num1))
-list2 = list_num2 + [0] * (max_len - len(list_num2))
+    result = [extended_list1[i] + extended_list2[i] for i in range(max_len)]
 
-result = [list1[i] + list2[i] for i in range(max_len)]
+    return result
 
-print(result) #[243, 439, 822, 1499, 114]
+# Пример:
+list_num1 = [120, 220, 330, 766, 333]
+list_num2 = [111, 211, 411, 11]
+
+result = sum_lists(list_num1, list_num2)
+print(result)  #[231, 431, 741, 777, 333]
