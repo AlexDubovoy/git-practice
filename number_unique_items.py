9fbfaba19cb0958703bde4026f8f944b0e2cdc8d
AlexDubovoy <dubovoy.alex91@gmail.com>
2026-03-30 16:46:02 +0300

    add number_unique_items.py with code

---
+++ number_unique_items.py
@@ -0,0 +1,8 @@
+numbers = [1, 3, 2, 2, 3, 5,  4, 6,  5, 9, 4, 4, 4, 6, 1, 1, 11, 0, 7 ]
+
+unique_count = len(set(numbers))
+unique_elements = set(numbers)
+
+print("Список:", numbers)
+print("Уникальные элементы:", unique_elements)
+print("Количество уникальных элементов:", unique_count)
