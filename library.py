a685245c6820b927adfdf25e5ec5b9d20ffe5dcf
AlexDubovoy <dubovoy.alex91@gmail.com>
2026-04-01 04:49:46 +0300

    add library.py with code

--- library.py
+++ library.py
@@ -1,8 +1,22 @@
+def book_list_view(library):
+    """Выводит названия всех книг в библиотеке.
+    Если книг нет — выводит сообщение об этом."""
+
+    if not library:
+        print("В библиотеке нет книг.")
+        return
+
+    print("Список книг в библиотеке:")
+
+    for i, title in enumerate(library.keys(), 1):
+        print(f"{i}. {title}")
+
+
 library = {
     "Alice's Adventures in Wonderland": {
-    'author': 'Lewis Carroll',
-    'year_of_publication': 1865,
-    'availability': True
+        'author': 'Lewis Carroll',
+        'year_of_publication': 1865,
+        'availability': True
     },
     "Harry Potter and the Philosopher's Stone": {
         'author': 'J.K. Rowling',
@@ -10,22 +24,6 @@ library = {
         'availability': False
     }
 }
-
-def book_list_view(library):
-    """
-    Выводит названия всех книг в библиотеке.
-    Если в библиотеке нет книг, функция выводит сообщение об этом.
-    """
-    if not library:
-        print("В библиотеки нет книг.")
-        return
-
-    print("Список книг в библиотеке:")
-
-    number = 1
-    for title in library:
-        print(f"{number}. {title}")
-        number += 1
 
 book_list_view(library)
 
